72f341d9f26a8e7baa47ffd598efa37850fbb819
Zoey6zzx <98821820+Zoey6zzx@users.noreply.github.com>
2022-04-20 14:23:08 +0200

    model Post

---
+++ src/models/Post.js
@@ -0,0 +1,8 @@
+export class Post {
+    constructor(id, title, content, author) {
+        this.author = author;
+        this.content = content;
+        this.id = id;
+        this.title = title;
+    }
+}
